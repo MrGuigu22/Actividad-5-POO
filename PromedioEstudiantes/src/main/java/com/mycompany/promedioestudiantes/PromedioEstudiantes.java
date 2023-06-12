de435dffd34bfb2dab2e7b0bf13a55e7aa38651e
MrGuigu22 <santiagomier22@gmail.com>
2023-06-12 17:53:15 -0500

    a

---
+++ PromedioEstudiantes/src/main/java/com/mycompany/promedioestudiantes/PromedioEstudiantes.java
@@ -0,0 +1,10 @@
+package com.mycompany.promedioestudiantes;
+
+public class PromedioEstudiantes {
+
+    public static void main(String[] args) {
+        VentanaPrincipa miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipa(); 
+        miVentanaPrincipal.setVisible(true);
+    }
+}
